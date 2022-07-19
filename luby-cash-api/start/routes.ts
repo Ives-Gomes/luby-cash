f3f418402f42ad77f3b72ba29d8f2ad835a89a71
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-07-19 20:14:05 -0300

    feat: auth configs and middlewares

--- luby-cash-api/start/routes.ts
+++ luby-cash-api/start/routes.ts
@@ -13,3 +13,15 @@ Route.get('test_db_connections', async ({ response }: HttpContextContract) => {
     return response.status(500).json({ message })
   })
 })
+
+Route.group(() => {
+  Route.post('login', 'AuthController.login')
+
+  Route.post('users/', 'UsersController.store')
+}).prefix('v1/api')
+
+Route.group(() => {
+  Route.resource('users/', 'UsersController').only(['update', 'index', 'show', 'destroy'])
+})
+  .prefix('v1/api')
+  .middleware(['auth', 'is:admin'])
